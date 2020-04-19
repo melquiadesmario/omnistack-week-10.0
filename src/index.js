962cf6efae49c464ff740c9a39d31cca7f61409b
melquiadesmario <melquiadesm@gmail.com>
2020-04-19 19:49:32 -0300

    Create and List Devs

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const mongoose = require('mongoose')
+
+const routes = require('./routes')
+
+const app = express()
+const port = process.env.PORT || 3333
+const URL_DB = 'mongodb+srv://melmario:melmario1234@cluster0-j2h1y.mongodb.net/omnistack10?retryWrites=true&w=majority'
+
+mongoose.connect(URL_DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then(() => console.log('DB Connected'))
+    .catch(err => console.log('FAILD DB', err))
+
+app.use(express.json())
+app.use(routes)
+
+app.listen(port, () => console.log(`Server is runing on port ${ port }`))
