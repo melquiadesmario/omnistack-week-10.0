c56d0ca1555f62c0b9a53eace240eca9349580fc
melquiadesmario <melquiadesm@gmail.com>
2020-05-26 21:42:27 -0300

    Socket IO

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
+const http = require('http')
 
 const routes = require('./routes')
+const { setupWebsocket } = require('./websocket')
 
 const app = express()
+const server = http.Server(app)
+
+setupWebsocket(server)
+
 const port = process.env.PORT || 3333
 const URL_DB = 'mongodb+srv://melmario:melmario1234@cluster0-j2h1y.mongodb.net/omnistack10?retryWrites=true&w=majority'
 
+mongoose.set('useCreateIndex', true)
 mongoose.connect(URL_DB, {
     useNewUrlParser: true,
     useUnifiedTopology: true
@@ -19,4 +26,4 @@ app.use(cors())
 app.use(express.json())
 app.use(routes)
 
-app.listen(port, () => console.log(`Server is runing on port ${ port }`))
+server.listen(port, () => console.log(`Server is runing on port ${ port }`))
